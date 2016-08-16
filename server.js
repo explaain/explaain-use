e3bdc6810c518ca888b2eb0f74f4bd99800d41a5
Iain Collins <me@iaincollins.com>
2016-08-16 14:58:06 +0100

    Wrapping in node.js app so it can run on heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+    partials = require('express-partials'),
+    app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
