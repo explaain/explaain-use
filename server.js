c400fa4fe6cf9720011b7e6f063038706976f42c
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-02-03 01:27:42 +0000

    Demo Page attempt

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/demo/:URL', function(request, response) {
+  response.render('pages/demo', { embedURL: request.params.URL });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
